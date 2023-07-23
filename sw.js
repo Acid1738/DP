1295e076e3387e636312bb4b27658b958a073baf
Acid1738 <117107842+Acid1738@users.noreply.github.com>
2023-07-23 15:37:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,8 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticfile).then(cache => {
-      cache.addAll(assets)
+      cache.addAll(assets);
+  console.log("assets are being cached")
     })
   )
 })
