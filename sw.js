fecff6d4c1e339d144dd7f65b5d143f8e23cb7b3
Acid1738 <117107842+Acid1738@users.noreply.github.com>
2023-09-13 18:39:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,19 +35,6 @@ self.addEventListener("fetch", fetchEvent => {
     })
   }
 
-function showNotification() {
-  Notification.requestPermission().then((result) => {
-    if (result === "granted") {
-      navigator.serviceWorker.ready.then((registration) => {
-        registration.showNotification("Vibration Sample", {
-          body: "Buzz! Buzz!",
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: "vibration-sample",
-        });
-      });
-    }
-  });
-}
- showNotification()
+
 
 
